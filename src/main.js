75a24a4ab506ad9c8d7f734f2034dc1bc1098442
TheChoco1 <khaheersaza@gmail.com>
2023-02-01 18:29:15 -0500

    added media costs test case

--- src/main.js
+++ src/main.js
@@ -7,4 +7,5 @@ console.log(`Attendance -> ${att}`);
 console.log(`Max Ticket profit -> ${Math.floor(maxTicketProfit(40.99, 55.99, 50.99, 75.99, 70.99, 87.99))}`);
 console.log(`Max Conession profit -> ${Math.floor(maxConcessionsProfit(att, 7, 4))}`);
 console.log(`Staffing -> ${calcStaff(att)}`);
+console.log(`Media Costs: ${mediaCosts(20000, 0, 55000, 50000)}`)
 console.log(`Salary -> ${playerSalary(2800880)}`);
