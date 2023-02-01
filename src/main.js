5339b489e754380e4c3be15d78e6d1d958bb4d81
TheChoco1 <khaheersaza@gmail.com>
2023-02-01 18:26:15 -0500

    made test case message prettier

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import { calcAttendance, maxTicketProfit, maxConcessionsProfit, calcStaff, media
 let att = calcAttendance(20000)
 
 // game calculations
-console.log(`Attendance - ${att}`);
-console.log(`Max Ticket profit - ${Math.floor(maxTicketProfit(40.99, 55.99, 50.99, 75.99, 70.99, 87.99))}`);
-console.log(`Max Conession profit - ${Math.floor(maxConcessionsProfit(att, 7, 4))}`);
-console.log(`Staffing - ${calcStaff(att)}`);
-console.log(`Salary - ${playerSalary(2800880)}`);
+console.log(`Attendance -> ${att}`);
+console.log(`Max Ticket profit -> ${Math.floor(maxTicketProfit(40.99, 55.99, 50.99, 75.99, 70.99, 87.99))}`);
+console.log(`Max Conession profit -> ${Math.floor(maxConcessionsProfit(att, 7, 4))}`);
+console.log(`Staffing -> ${calcStaff(att)}`);
+console.log(`Salary -> ${playerSalary(2800880)}`);
