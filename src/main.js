b05783c13436319c6c5cc25c3569dc96c8741377
TheChoco1 <khaheersaza@gmail.com>
2023-01-31 20:15:27 -0500

    add test cases

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { calcAttendance } from "./game.js";
+import { calcAttendance, maxTicketProfit, maxConcessionsProfit, calcStaff, mediaCosts, playerSalary, totalGameProfit } from "./game.js";
 
 let att = calcAttendance(20000)
+
 // game calculations
-console.log(`Attendance: ` + att);
+console.log(`Attendance: ${att}`);
+console.log(`Max Ticket profit: ${Math.floor(maxTicketProfit(40.99, 55.99, 50.99, 75.99, 70.99, 87.99))}`);
+console.log(`Max Conession profit: ${Math.floor(maxConcessionsProfit(att, 7, 4))}`);
+console.log(`Salary: ${playerSalary(2800880)}`);
