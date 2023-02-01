48d5cccc82b380ff2fe8ff564ee95566d803a1fa
TheChoco1 <khaheersaza@gmail.com>
2023-01-31 20:17:58 -0500

    cleaned up code to reduce amount of lines+optimize

--- src/game.js
+++ src/game.js
@@ -1,71 +1,58 @@
 // Calculators
 
-export const calcAttendance = (att) => {
+const calcAttendance = (att) => {
     if (att*1.25>19008) return 19008;
     else return att*1.25;
 };
 
-export const maxTicketProfit = (ULST, ULMT, MLST, MLMT, LLST, LLMT) => {
+const maxTicketProfit = (ULST, ULMT, MLST, MLMT, LLST, LLMT) => {
 
     // Ticket Profit var declarations
-    let ULSP = 0;
-    let ULMP = 0;
-    let MLSP = 0;
-    let MLMP = 0;
-    let LLSP = 0;
-    let LLMP = 0;
-    
-    // Each section profit
-    ULSP = ULST * 3168;
-    ULMP = ULMT * 3168;
-    MLSP = MLST * 3168;
-    MLMP = MLMT * 3168;
-    LLSP = LLST * 3168;
-    LLMP = LLMT * 3168;
+    let ULSP = ULST * 3168;
+    let ULMP = ULMT * 3168;
+    let MLSP = MLST * 3168;
+    let MLMP = MLMT * 3168;
+    let LLSP = LLST * 3168;
+    let LLMP = LLMT * 3168;
 
     return (
         ULSP + ULMP + MLSP + MLMP + LLSP + LLMP
     );
 }
 
-const maxConcessionsProfit = (att, amountOfStands) => {
+const maxConcessionsProfit = (att, amountOfStands, employeePerStand) => {
     
-    // Concessions Profit var declarations
-    let popUnit = att * .4;
-    let iceUnit = att * .5;
-    let pretzelsUnit = att * .2;
-    let bottledUnit = att * .4;
-    let fountainUnit = att * .6;
-    let chipsUnit = att * .2;
+    // unit calc
+    let popUnit = (att * .4)/.5;
+    let iceUnit = (att * .5)/.5;
+    let pretzelsUnit = (att * .2)/.5;
+    let bottledUnit = (att * .4)/.5;
+    let fountainUnit = (att * .6)/.5;
+    let chipsUnit = (att * .2)/.5;
 
-    let popPrice = 13.0;
-    let icePrice = 13.5;
-    let pretzelsPrice = 13;
-    let bottledPrice = 13.5;
-    let fountainPrice = 13.5;
-    let chipsPrice = 13;
+    // pricing calc
+    let popPrice = 80000/(popUnit);
+    let icePrice = 100000/(iceUnit);
+    let pretzelsPrice = 40000/(pretzelsUnit);
+    let bottledPrice = 80000/(bottledUnit);
+    let fountainPrice = 110000/(fountainUnit);
+    let chipsPrice = 40000/(chipsUnit);
 
-    let popPro = 0;
-    let icePro = 0;
-    let prePro = 0;
-    let botPro = 0;
-    let fouPro = 0;
-    let chipPro = 0;
-    let conStandCost = 0;
+    // profit calc
+    let popPro = ((popUnit) * popPrice) - ((popUnit) * .86);
+    let icePro = ((iceUnit) * icePrice) - ((iceUnit) * .95);
+    let prePro = ((pretzelsUnit) * pretzelsPrice) - ((pretzelsUnit) * .83);
+    let botPro = ((bottledUnit) * bottledPrice) - ((bottledUnit) * .95);
+    let fouPro = ((fountainUnit) * fountainPrice) - ((fountainUnit) * .72);
+    let chipPro = ((chipsUnit) * chipsPrice) - ((chipsUnit) * .69);
 
-    //profits
-    popPro = ((popUnit) * popPrice) - ((popUnit) * .86);
-    icePro = ((iceUnit) * icePrice) - ((iceUnit) * .95);
-    prePro = ((pretzelsUnit) * pretzelsPrice) - ((pretzelsUnit) * .83);
-    botPro = ((bottledUnit) * bottledPrice) - ((bottledUnit) * .95);
-    fouPro = ((fountainUnit) * fountainPrice) - ((fountainUnit) * .72);
-    chipPro = ((chipsUnit) * chipsPrice) - ((chipsUnit) * .69);
-
-    // stand costs (NEED TO ADD STAFF OF STAND COSTS)
-    conStandCost = 3100*amountOfStands;
+    // staff costs calc
+    let conStandCost = 3300*amountOfStands;
+    let conStaffCost = (employeePerStand*amountOfStands)*1524.60714;
 
     return (
-        popPro + icePro + prePro + botPro + fouPro + chipPro - conStandCost
+        // adding together profits of each indiv and subtracting stand costs
+        popPro + icePro + prePro + botPro + fouPro + chipPro - conStandCost - conStaffCost
     );
 }
 
@@ -99,3 +86,5 @@ const mediaCosts = (SM, np, radio, tv) => SM + np + radio + tv;
 const playerSalary = (fullSalary) => fullSalary/16;
 
 const totalGameProfit = () => maxConcessionsProfit(calcAttendance(5874), 7) + maxTicketProfit(40.99, 50.99, 60.99, 65.99, 75.99, 84.99) - mediaCosts(80000, 81000) - playerSalary(2800088) - 400000 + 250000 - 40000;
+
+export { calcAttendance, maxTicketProfit, maxConcessionsProfit, calcStaff, mediaCosts, playerSalary, totalGameProfit };
